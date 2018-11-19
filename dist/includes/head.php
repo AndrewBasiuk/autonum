41da6c922867ffb5bad8c76828aa5aad7897a073
Andrew Basiuk <a.basyuk4779@gmail.com>
2018-11-20 00:36:07 +0200

    first commit

---
+++ dist/includes/head.php
@@ -0,0 +1,67 @@
+<div class="head">
+	<header class="header">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 nav-wrap">
+					<div class="lang">
+						<button>
+							<i class="fas fa-globe"></i>
+							<span>Русский</span>
+						</button>
+						<ul>
+							<li><a href="#">Украинский</a></li>
+							<li><a href="#">Русский</a></li>
+						</ul>
+					</div>
+					<nav class="header__nav_top">
+						<ul>
+							<li><a href="#">Оплата</a></li>
+							<li><a href="#">Доставка</a></li>
+							<li><a href="#">Образцы</a></li>
+							<li><a href="#">Вопросы и ответы</a></li>
+							<li><a href="#">Полезное</a></li>
+							<li><a href="#">Статьи</a></li>
+							<li><a href="#">Контакты</a></li>
+						</ul>
+					</nav>
+				</div>
+				<div class="col-md-3 offset-md-1 header__contact">
+					<ul>
+						<li><a href="#"><i class="fa fa-phone" aria-hidden="true"></a></i></li>
+						<li><a href="#"><i class="fab fa-viber"></i></a></li>
+						<li><a href="#"><i class="fab fa-whatsapp"></i></a></li>
+					</ul>
+					<a href="#">(097) 455 55 77</a>
+				</div>
+			</div>
+		</div>
+	</header>
+	<div class="container">
+		<img class="logo" src="img/head-auto.jpg" alt="logo">
+		<nav class="header__nav_bottom">
+			<ul>
+				<li><a href="#"><i class="fas fa-home"></i></a></li>
+				<li><a href="#">Заказ Онлайн</a></li>
+				<li><a href="#">Дубликаты номеров</a></li>
+				<li><a href="#">Утеря номеров?</a></li>
+				<li><a href="#">Востановление номеров</a></li>
+				<li><a href="#">Американский размер</a></li>
+				<li><a href="#">Сувенирные номера</a></li>
+				<li><a href="#">Именные номера</a></li>
+				<li><a href="#">Блатные номера</a></li>
+				<li><a href="#">Рамки</a></li>
+			</ul>
+		</nav>
+
+		<div class="row justify-content-end">
+			<ul class="col-2 social">
+			<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+			<li><a href="#"><i class="fab fa-vk"></i></a></li>
+			<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+			<li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+			<li><a href="#"><i class="fas fa-envelope"></i></a></li>
+		</ul>
+		</div>
+		<h1 class="h1">Заказать изготовление номеров и автомобильных номерных знаков (автономеров) с <br> доставкой по Киеву, всей Украине. Купить или сделать номера машины в Киеве.</h1>
+	</div>
+</div>
