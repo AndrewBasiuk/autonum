0979fa6e977b9e13ee026a53fd0452743125c362
Andrew Basiuk <andrii.basiuk@mgid.com>
2018-12-17 18:59:20 +0200

    start souvenir.php

---
+++ dist/souvenir.php
@@ -0,0 +1,270 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/custom-style.css">
+
+
+    <title>avtonum</title>
+  </head>
+  <body>
+    
+    <?php include_once "includes/head.php"; ?>
+
+    <div class="table-wrap">
+      <div class="container">
+        <div class="table">
+          <div class="table-row table-head">
+            <div class="table-type">
+              <p class="table__text">Тип номера</p>
+            </div>
+            <div class="table-price">
+              <p class="table__text">Стоимость <br> 1 номера</p>
+            </div>
+            <div class="table-price">
+              <p class="table__text">
+                Стоимость <br> комплекта
+              </p>
+            </div>
+            <div class="table-button">
+            </div>
+          </div>
+          <div class="table-row">
+            <div class="table-type">
+              <div class="table-image">
+                <img src="img/suv_1.jpg" title="avto-num" alt="avto-num">
+              </div>
+              <div class="table-link">
+                <a href="#">Сувенирный алюминиевый номер без наклеек</a>
+              </div>
+            </div>
+            <div class="table-price">
+              <p class="table__text">
+                <span class="table__text_mobile">1 шт - </span>
+                <span class="table__text_big"> 250 </span>
+                грн
+              </p>
+            </div>
+            <div class="table-price">
+              <p class="table__text">
+                <p class="table__text">
+                  <span class="table__text_mobile">2 шт - </span>
+                  <span class="table__text_big"> 400 </span>
+                  грн
+                </p>
+              </p>
+            </div>
+            <div class="table-button">
+              <button>заказать</button>
+            </div>
+          </div>
+          <div class="table-row">
+            <div class="table-type">
+              <div class="table-image">
+                <img src="img/num_2.jpg" title="avto-num" alt="avto-num">
+              </div>
+              <div class="table-link">
+                <a href="#">Сувенирный алюминиевый номер с наклейкой</a>
+              </div>
+            </div>
+            <div class="table-price">
+              <p class="table__text">
+                <span class="table__text_mobile">1 шт - </span>
+                <span class="table__text_big">300</span>
+                грн
+              </p>
+            </div>
+            <div class="table-price">
+              <p class="table__text">
+                <p class="table__text">
+                  <span class="table__text_mobile">2 шт - </span>
+                  <span class="table__text_big">450</span>
+                  грн
+                </p>
+              </p>
+            </div>
+            <div class="table-button">
+              <button>заказать</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="content-block content-block_attention">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 content-block-image">
+            <img src="img/atention.jpg" title="doc-image" alt="doc-image">
+          </div>
+          <div class="col-md-8">
+            <p class="paragraph">
+              <b>Обращаем ваше внимание:</b>
+            </p>
+            <p class="paragraph">
+              Иммитация украинских госномеров запрещена !
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="content-block">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 content-block-image">
+            <img src="img/suv_3.jpg" title="doc-image" alt="doc-image">
+          </div>
+          <div class="col-md-8">
+            <h2 class="h2">
+              <b>Алюминиевые сувенирные номера</b>
+            </h2>
+
+            <p class="paragraph">
+              Сувенирные автономера на алюминиевой основе изготовляются методом выдавливания, благодаря чему символы на номере четкие и глубокие. Для таких номеров можно использовать все символы кириллицы, латиницы и цифр. Сувенирные номерные знаки делают на алюминиевой пластине со светоотражащим покрытием, используются такие же шрифты, как и на официальных гос номерах. На алюминиевом сувенирном номере можно использовать до восьми символов и нанести одно или несколько изображений с помощью высококачественной самоклеющейся пленки.
+            </p>
+            
+            <div class="souvenir-slider-wrap">
+              <p class="paragraph"><b>Примеры и образцы сувенирных номеров:</b></p>
+              <ul class="row slider mobile-slider souvenir-slider">
+                <li class="col-md-3"><img src="img/samples/1_1.jpg" title="num-image" alt="num-image"></li>
+                <li class="col-md-3"><img src="img/samples/1_2.jpg" title="num-image" alt="num-image"></li>
+                <li class="col-md-3"><img src="img/samples/1_3.jpg" title="num-image" alt="num-image"></li>
+                <li class="col-md-3"><img src="img/samples/1_4.jpg" title="num-image" alt="num-image"></li>
+                <li class="col-md-3"><img src="img/samples/1_1.jpg" title="num-image" alt="num-image"></li>
+                <li class="col-md-3"><img src="img/samples/1_2.jpg" title="num-image" alt="num-image"></li>
+                <li class="col-md-3"><img src="img/samples/1_3.jpg" title="num-image" alt="num-image"></li>
+                <li class="col-md-3"><img src="img/samples/1_4.jpg" title="num-image" alt="num-image"></li>
+                <li class="col-md-3"><img src="img/samples/1_1.jpg" title="num-image" alt="num-image"></li>
+              </ul>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="content-block content-block_yellow">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 content-block-image">
+            <img src="img/postman.jpg" title="doc-image" alt="doc-image">
+          </div>
+          <div class="col-md-8">
+            <h2 class="h2">
+              <b>Заказать номерной знак с доставкой по Киеву и всей Украине</b>
+            </h2>
+
+            <p class="paragraph">
+              Вы можете заказать номерной знак с доставкой по Киеву, наш курьер доставит ваш заказ по адресу в течении дня, либо если необходимо, мы отправим заказанные номерные знаки Новой Почтой в ваш населенный пункт.
+            </p>
+            <p class="paragraph">
+              Доставка выполняется нашими на протяжении рабочего дня, оплатить заказ можно курьеру при получении. Чтобы ваш заказ мы смогли выполнить в этот же день, его необходимо оформить до 14:00, заказы с доставкой оформленные после 14:00 переносятся на следующий день.
+            </p>
+            <p class="paragraph">
+              Доставка по Украине службой Новая Почта. Заказ приходит на склад перевозчика в вашем городе. Кроме того вы можете заказать номера с оплатой наложенным платежом при получении в вашем городе, в этом случае к стоимости заказа добавится 30-35 грн за обратную отправку денег.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="content-block">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 content-block-image">
+            <img src="img/img_1.jpg" title="doc-image" alt="doc-image">
+          </div>
+          <div class="col-md-8">
+            <h2 class="h2">
+              <b>Изготовление автономеров в Киеве</b>
+            </h2>
+
+            <p class="paragraph">
+              Каждый месяц в Украине теряется несколько тысяч номерных знаков. Это не маленькая цифра. Производство номерных знаков осуществляют сертифицированные заводы-изготовители, которых на территории нашей страны около 6. Государственный номерной знак можно потерять, повредить, к сожалению, могут украсть прямо с машины, в конце концов автономера могут прийти в негодность в связи с долговременной эксплуатацией, под действием времени и коррозии. Естественно, в таком случае автовладелец в первую очередь обращается в ГАИ. Однако в связи с рядом объективных причин, органы ГАИ не занимаются изготовлением дубликатов номерных гос знаков, а лишь могут выдать новый номер с заменой техпаспорта. Как следствие владельца машины посещает вопрос: где я куплю номер?
+            </p>
+            <p class="paragraph">
+              В Украине официально разрешено изготовление номеров на автомобиль фирмами-посредниками, то есть производство и покупка номеров в Украине разрешены законодательством. Покупка государственного автономера выгодна и удобна для водителя тем, что ему нет необходимости делать замену техпаспорта и как следствие замену страховки и прочих документов, нет необходимости заниматься всей этой утомительной и дорогостоящей процедурой.
+            </p>
+            <p class="paragraph">
+              Обращаясь к нам (ЧП "Госзнаки" Киев, Украина), вы можете заказать автономер он-лайн, с доставкой в любой город Украины. Мы можем сделать практический любой тип автомобильного номерного знака в кратчайшие сроки, в т.ч. и русские номера для граждан России. Помимо номеров на автомобиль, мы делаем номера на мотоциклы и прицепы. Так же у нас вы можно сделать заказ заднего номера американского стандарта. Кроме того, мы делаем сувенирные и именные номера машин на заказ. 
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="ukraine-order">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 content-block-image">
+            <img src="img/ukraine-map.jpg" title="ukraine-map" alt="ukraine-map">
+          </div>
+          <div class="col-md-8">
+            <h2 class="h2">
+              <b>Заказать номерные знаки в наших представительствах по Украине</b>
+            </h2>
+            <div class="row">
+              <div class="col-sm-6">
+                <ul class="city-link-list">
+                  <li><a href="#">Изготовление номеров Винница</a></li>
+                  <li><a href="#">Изготовление номеров Днепропетровск</a></li>
+                  <li><a href="#">Изготовление номеров Донецк</a></li>
+                  <li><a href="#">Изготовление номеров Житомир</a></li>
+                  <li><a href="#">Изготовление номеров Запорожье</a></li>
+                  <li><a href="#">Изготовление номеров Ивано-Франковск</a></li>
+                  <li><a href="#">Изготовление номеров Кировоград</a></li>
+                  <li><a href="#">Изготовление номеров Кривой Рог</a></li>
+                  <li><a href="#">Изготовление номеров Луганск</a></li>
+                  <li><a href="#">Изготовление номеров Луцк</a></li>
+                  <li><a href="#">Изготовление номеров Львов</a></li>
+                  <li><a href="#">Изготовление номеров Николаев</a></li>
+                  <li><a href="#">Изготовление номеров Одесса</a></li>
+                </ul>
+              </div>
+              <div class="col-sm-6">
+                <ul class="city-link-list">
+                  <li><a href="#">Изготовление номеров Полтава</a></li>
+                  <li><a href="#">Изготовление номеров Ровно</a></li>
+                  <li><a href="#">Изготовление номеров Симферополь</a></li>
+                  <li><a href="#">Изготовление номеров Сумы</a></li>
+                  <li><a href="#">Изготовление номеров Тернополь</a></li>
+                  <li><a href="#">Изготовление номеров Ужгород</a></li>
+                  <li><a href="#">Изготовление номеров Харьков</a></li>
+                  <li><a href="#">Изготовление номеров Херсон</a></li>
+                  <li><a href="#">Изготовление номеров Хмельницкий</a></li>
+                  <li><a href="#">Изготовление номеров Черкассы</a></li>
+                  <li><a href="#">Изготовление номеров Чернигов</a></li>
+                  <li><a href="#">Изготовление номеров Черновцы</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <?php include_once "includes/footer.php"; ?>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+   <!--  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> -->
+
+    <script src="js/common.js"></script>
+    <script src="js/home.js"></script>
+
+    <script src="js/custom.js"></script>
+
+
+  </body>
+</html>
