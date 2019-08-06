4ab8d2d3f0222d4a493301b896b298600955f87b
Andrew Basiuk <andrii.basiuk@mgid.com>
2019-08-06 21:04:51 +0300

    commit

--- 2/dist/includes/head.php
+++ 2/dist/includes/head.php
@@ -6,14 +6,7 @@
 					<img src="img/icons/icon_menu_gamburger.png" title="hamburger-icon" alt="hamburger-icon">
 					<span>МЕНЮ</span>
 				</div>
-				<div class="row nav-wrap">
-					<div class="hamburger-close">
-						<svg width="64" version="1.1" xmlns="http://www.w3.org/2000/svg" height="64" viewBox="0 0 64 64" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 64 64">
-						<g>
-							<path d="M28.941,31.786L0.613,60.114c-0.787,0.787-0.787,2.062,0,2.849c0.393,0.394,0.909,0.59,1.424,0.59   c0.516,0,1.031-0.196,1.424-0.59l28.541-28.541l28.541,28.541c0.394,0.394,0.909,0.59,1.424,0.59c0.515,0,1.031-0.196,1.424-0.59   c0.787-0.787,0.787-2.062,0-2.849L35.064,31.786L63.41,3.438c0.787-0.787,0.787-2.062,0-2.849c-0.787-0.786-2.062-0.786-2.848,0   L32.003,29.15L3.441,0.59c-0.787-0.786-2.061-0.786-2.848,0c-0.787,0.787-0.787,2.062,0,2.849L28.941,31.786z"/>
-						</g>
-						</svg>
-					</div>
+				<div class="nav-wrap">
 					<nav class="header__nav">
 						<ul>
 							<li><a href="#">Оплата</a></li>
@@ -36,35 +29,15 @@
 						</ul>
 					</nav>
 				</div>
-				<div class="offset-md-1 col-md-3 header__contact" style="display: none;">
-					<ul>
-						<li>
-							<a href="#">
-								<img src="img/icons/icon_phone.png" title="phone-icon" alt="phone-icon">
-							</a>
-						</li>
-						<li>
-							<a href="#">
-								<img src="img/icons/viber.png" title="viber-icon" alt="viber-icon">
-							</a>
-						</li>
-						<li>
-							<a href="#">
-								<img src="img/icons/whatsapp.png" title="whatsapp-icon" alt="whatsapp-icon">
-							</a>
-						</li>
-					</ul>
-					<a href="#">(097) 455 55 77</a>
-				</div>
 			</div>
 		</div>
 	</header>
 
-	<div class="head-content">
+	<div class="head-middle">
 		<div class="container">
-			<div class="row">
-				<div class="row col-6 col-md-4 head-content_left">
-					<div class="col-9 logo">
+			<div class="row justify-content-between head-midlle">
+				<div class="row col-12 col-md-4 head-middle_left">
+					<div class="col-12 col-md-9 logo">
 						<img src="img/logo.png" alt="logo">
 					</div>
 					<div class="col-3 lang">
@@ -83,7 +56,7 @@
 						</ul>
 					</div>
 				</div>
-				<div class="col-6 col-md-5 offset-md-3 head-content_right">
+				<div class="col-12 col-md-8 col-lg-5 offset-lg-3 head-middle_right">
 					<div class="contact header__contact">
 						<ul class="contact_top">
 							<li class="contact__item">
@@ -93,7 +66,7 @@
 							</li>
 							<li class="contact__item">
 								<a href="#">
-									<img src="img/icons/whatsapp.png" title="whatsapp-icon" alt="whatsapp-icon">
+									<img src="img/icons/telegram.png" title="telegram-icon" alt="telegram-icon">
 								</a>
 							</li>
 							<li class="contact__item">
@@ -121,27 +94,57 @@
 						</ul>
 						<div class="contact_bottom"></div>
 					</div>
+					<a href="#" class="head__order">Он-лайн заказ</a>
 				</div>
+			</div>
+
+			<nav class="nav-mobile">
+				<p class="nav-mobile__heading"></p>
+				<ul>
+					<li><a href="#">Оплата</a></li>
+					<li><a href="#">Доставка</a></li>
+					<li><a href="#">Необходимые документы</a></li>
+					<li><a href="#">Как заказать</a></li>
+					<li><a href="#">Номера регионов, типы номеров</a></li>
+					<li><a href="#">Новости</a></li>
+				</ul>
+				<ul class="header__nav_mobile">
+					<li><a href="#">Заказ Онлайн</a></li>
+					<li><a href="#">Дубликаты номеров</a></li>
+					<li><a href="#">Утеря номеров?</a></li>
+					<li><a href="#">Востановление номеров</a></li>
+					<li><a href="#">Американский размер</a></li>
+					<li><a href="#">Сувенирные номера</a></li>
+					<li><a href="#">Именные номера</a></li>
+					<li><a href="#">Блатные номера</a></li>
+					<li><a href="#">Рамки</a></li>
+				</ul>
+			</nav>
+		</div>
+	</div>
+
+	<div class="head-bottom">
+		<div class="container">
+			<div class="row">
+				<nav class="col-12 header__nav_bottom">
+					<ul>
+						<li><a href="#"><i class="fas fa-home"></i></a></li>
+						<li><a href="#">Дубликаты номеров</a></li>
+						<li><a href="#">Изготовление номеров</a></li>
+						<li><a href="#">Восстановление номеров</a></li>
+						<li><a href="#">Утеряны номера</a></li>
+						<li><a href="#">Американский размер</a></li>
+						<li><a href="#">Образцы</a></li>
+						<li><a href="#">Цены</a></li>
+						<li><a href="#">Контакты</a></li>
+						<!-- <li><a href="#">Рамки</a></li> -->
+					</ul>
+				</nav>
 			</div>
 		</div>
 	</div>
 
 	<div class="container">
-		<nav class="header__nav_bottom">
-			<ul>
-				<li><a href="#"><i class="fas fa-home"></i></a></li>
-				<li><a href="#">Заказ Онлайн</a></li>
-				<li><a href="#">Дубликаты номеров</a></li>
-				<li><a href="#">Утеря номеров?</a></li>
-				<li><a href="#">Востановление номеров</a></li>
-				<li><a href="#">Американский размер</a></li>
-				<li><a href="#">Сувенирные номера</a></li>
-				<li><a href="#">Именные номера</a></li>
-				<li><a href="#">Блатные номера</a></li>
-				<li><a href="#">Рамки</a></li>
-			</ul>
-		</nav>
-		
 		<div class="row justify-content-end soc-wrap">
 			<ul class="col-2 social">
 				<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
@@ -151,7 +154,8 @@
 				<li><a href="#"><i class="fas fa-envelope"></i></a></li>
 			</ul>
 		</div>
+
+		<h1 class="h1">Заказать изготовление номеров и автомобильных номерных знаков (автономеров) с <br> доставкой по Киеву, всей Украине. Купить или сделать номера машины в Киеве.</h1>
 	</div>
 
-	<h1 class="h1">Заказать изготовление номеров и автомобильных номерных знаков (автономеров) с <br> доставкой по Киеву, всей Украине. Купить или сделать номера машины в Киеве.</h1>
 </section>
